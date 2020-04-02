0b5d95feac00ff673a5b934a84b7d67ee27976d6
DrX7FFF <dubourg.v@gmail.com>
2020-04-02 11:55:13 +0200

    Mineur : Mise à jour Lettre de version perso

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SKR-min-E3_V1.2 K"
+#define CUSTOM_MACHINE_NAME "SKR-min-E3_V1.2 L"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
